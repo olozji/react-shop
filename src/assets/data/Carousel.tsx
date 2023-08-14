4f7e429d1dcd5c38e75e3d63727e34a9fed4de6c
choiceyourj <eunji00126@gmail.com>
2023-08-14 21:25:33 +0900

    no message

--- src/assets/data/Carousel.tsx
+++ src/assets/data/Carousel.tsx
@@ -1,9 +1,9 @@
-export const slideData:any = [
+export const slideData = [
     {
         id:1,
         title:'최신 트렌드의 다양한 종류의 옷',
         content:'최신 유행 상품들을 만나보세요!',
-        src:'../../../public/carousel/fast-fashion-vs-slow-sustainable-fashion.jpg',
+        src:'/carousel/man-using-external-storage-used.jpg',
         alt: "image1",
         path:'/fashion'
     },
@@ -11,7 +11,7 @@ export const slideData:any = [
         id:2,
         title:'당신을 빛나게 해줄 악세사리',
         content:'Gift 를 위한 정성스런 포장까지!',
-        src:'../../../public/carousel/white-shopping-bag-gift-boxes-with-jewelry-colored-background.jpg',
+        src:'carousel/white-shopping-bag-gift-boxes-with-jewelry-colored-background.jpg',
         alt: "image2",
         path:'/accessory'
     },
@@ -19,7 +19,7 @@ export const slideData:any = [
         id:3,
         title:'품질이 검증된 디지털 기기',
         content:'직배송으로 하루 배송을 보장!',
-        src:'../../../public/carousel/man-using-external-storage-used.jpg',
+        src:'carousel/man-using-external-storage-used.jpg',
         alt: "image3",
         path:'/digital'
     },
