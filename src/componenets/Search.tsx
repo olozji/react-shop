c61bf3b59e4746a769aa0412f6107c2abdba5a86
choiceyourj <eunji00126@gmail.com>
2023-08-18 19:07:37 +0900

    no message

--- src/componenets/Search.tsx
+++ src/componenets/Search.tsx
@@ -1,7 +1,8 @@
-import React, {useEffect, useState} from 'react'
+import React, {useEffect, useRef, useState} from 'react'
 import { Link, useNavigate } from 'react-router-dom';
 import { useRecoilValue } from 'recoil';
 import { getData } from '../store/ProductsAtoms';
+import Products from '../pages/Products';
 
 interface ProductData {
   id: number;
@@ -27,6 +28,8 @@ const Search = (props: SearchProps) => {
   const [isSearch, setIsSearch] = useState(false);
   const fetchProductData = useRecoilValue(getData);
   const [fetchedProductData, setFetchedProductData] = useState<ProductData[]>([]);
+  const searchInputRef = useRef<HTMLInputElement>(null);
+
 
   const handleSearchChange = (event:React.ChangeEvent<HTMLInputElement>) => {
     
@@ -44,6 +47,11 @@ const Search = (props: SearchProps) => {
     navigate(`/products/${productId}`);
   };
 
+  const handleButtonClick = () => {
+   setIsSearch(!isSearch);
+  };
+
+
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -57,14 +65,35 @@ const Search = (props: SearchProps) => {
     fetchData();
   }, [fetchProductData]);
 
+  
 
   return (
     <div>
+       <button
+        className="btn btn-ghost btn-circle sm:hidden"
+        onClick={handleButtonClick}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-5 w-5"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+          />
+        </svg>
+      </button>
       <input 
       type={'search'}
       placeholder="검색"
       id="search"
       onChange={handleSearchChange}
+      ref={searchInputRef}
       className="input absolute w-full h-full rounded-none border border-slate-400 left-0 z-[-10] invisible sm:hidden transition ease-in-out duration-300"
       />
       <input
@@ -76,7 +105,7 @@ const Search = (props: SearchProps) => {
       />
 
     {isSearch && (
-     <ul className='menu dropdown-content p-2 shadow bg-base-100 mt-4 !fixed sm:!absolute sm:top-14 w-50 max-h-96 text-base-content overflow-y-auto'>
+     <ul className='menu dropdown-content p-2 shadow bg-base-100 mt-4 !fixed sm:!absolute sm!invisible sm:top-14 w-50 max-h-96 text-base-content overflow-y-auto'>
       {fetchedProductData.map((product:ProductData) => (
           <li key={product.id}>
             <Link 
